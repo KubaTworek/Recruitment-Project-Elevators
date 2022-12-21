d4f6fbd3313ffa0aed5f1b49ec72be10dfe45be2
KubaTworek <kubatvorek@gmail.com>
2022-12-21 01:39:09 +0100

    make primitive frontened

---
+++ web/app.js
@@ -0,0 +1,63 @@
+const APIURL = "http://localhost:9090/elevators";
+getElevetors();
+
+// ASYNC METHODS
+
+async function getElevetors() {
+    const resp = await fetch(APIURL);
+    const respData = await resp.json();
+
+    fillDivs(respData)
+}
+
+async function sendData(sourceFloor, destinationFloor) {
+    await fetch(APIURL + '/pickup?sourceFloor=' + sourceFloor + '&destinationFloor=' + destinationFloor, {method: 'post'});
+}
+
+async function step() {
+    clearDivs()
+
+    await fetch(APIURL + '/step', {method: 'put'});
+    await getElevetors()
+}
+
+// HELP METHODS
+
+function fillDivs(response){
+    for(let i=0; i<16; i++){
+        let firstClass = '.elevator-' + response[i].id
+        let secondClass = '.floor-' + response[i].numberOfFloor
+        const innerDiv = document
+        .querySelector(firstClass)
+        .querySelector(secondClass)
+        innerDiv.classList.add("taken")
+    }
+}
+
+function clearDivs(){
+    for(let i=1; i<17; i++){
+        for(let j=0; j<9; j++) {
+            let firstClass = '.elevator-' + i
+            let secondClass = '.floor-' + j
+            const innerDiv = document
+            .querySelector(firstClass)
+            .querySelector(secondClass)
+            innerDiv.classList.remove("taken")
+        }
+    }
+}
+
+// BUTTONS METHODS
+
+function getValues() 
+{
+    sourceFloor = document.getElementById('sourceFloor').value;
+    destinationFloor = document.getElementById('destinationFloor').value;
+
+    sendData(sourceFloor, destinationFloor)
+}
+
+function makeStep() 
+{
+    step()
+}
